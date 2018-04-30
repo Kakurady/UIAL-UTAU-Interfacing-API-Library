a39921dac63ecdeb05535f1191219b6c12ce330c
Kakurady <kakurady@gmail.com>
2018-04-30 17:53:31 -0400

    Stub out Tests project.

--- Tests/FileTest.cs
+++ Tests/FileTest.cs
@@ -8,8 +8,6 @@ using System.Linq;
 using zuoanqh.UIAL.VB;
 using zuoanqh.UIAL.UST;
 using zuoanqh.UIAL.Extensions.Mrq4Cs;
-using zuoanqh.libzut.FileIO;
-using zuoanqh.libzut.Data;
 //using zuoanqh.libzut;
 
 namespace zuoanqh.UIAL.Testing
@@ -21,32 +19,33 @@ namespace zuoanqh.UIAL.Testing
     public void Initialize()
     {
       Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);// because .net standard don't currently have sjis
-      Logger.LogDirectory = @"D:\UIAL_Logger";
+      //Logger.LogDirectory = @"D:\UIAL_Logger";
     }
     [TestMethod]
+    [Ignore]
     public void TestOto()
     {
-      //var a = PrefixMap.REGISTER_NAMES;
-      Stopwatch w = new Stopwatch();
-      string otos = Path.Combine(Directory.GetCurrentDirectory(), "dummyOtos");
-      List<Oto> lib = new List<Oto>();
+    //  //var a = PrefixMap.REGISTER_NAMES;
+    //  Stopwatch w = new Stopwatch();
+    //  string otos = Path.Combine(Directory.GetCurrentDirectory(), "dummyOtos");
+    //  List<Oto> lib = new List<Oto>();
 
-      Oto.CHECK_ENCODING = false;
-      foreach (string s in Directory.GetFiles(otos))
-      {
-        var o = new Oto(s);
-        lib.Add(o);
-        Logger.Log("File: " + Path.GetFileName(s));
-        Logger.Log("Encoding: " + zuio.GetEncUde(s));
-        Logger.Log("Alias: " + o.AliasCount);
-        Logger.Log("Extras: " + o.ExtrasCount);
-        //if (o.ExtrasCount > 0)
-          //Logger.Log("Extra Alias Counts: " + zusp.List(o.Extras.Keys.Select((k) => "\"" + k + "\" " + o.Extras[k].Count).ToArray()));
-        Logger.Log("=====================================");
-      }
-      Console.WriteLine("Time: " + w.Elapsed.TotalMilliseconds + "ms");
+    //  Oto.CHECK_ENCODING = false;
+    //  foreach (string s in Directory.GetFiles(otos))
+    //  {
+    //    var o = new Oto(s);
+    //    lib.Add(o);
+    //    Logger.Log("File: " + Path.GetFileName(s));
+    //    Logger.Log("Encoding: " + zuio.GetEncUde(s));
+    //    Logger.Log("Alias: " + o.AliasCount);
+    //    Logger.Log("Extras: " + o.ExtrasCount);
+    //    //if (o.ExtrasCount > 0)
+    //      //Logger.Log("Extra Alias Counts: " + zusp.List(o.Extras.Keys.Select((k) => "\"" + k + "\" " + o.Extras[k].Count).ToArray()));
+    //    Logger.Log("=====================================");
+    //  }
+    //  Console.WriteLine("Time: " + w.Elapsed.TotalMilliseconds + "ms");
 
-      Logger.Save();
+    //  Logger.Save();
     }
 
     [TestMethod]
@@ -64,65 +63,68 @@ namespace zuoanqh.UIAL.Testing
       }
     }
 
-    //[TestMethod]
+    [TestMethod]
+    [Ignore]
     public void TestUSTEncoding()
     {
-      string usts = @"D:\Test UST Archive";
-      TallyDictionary<Encoding> count = new TallyDictionary<Encoding>();
-      Stopwatch sw = new Stopwatch();
-      sw.Start();
-      int undetected = 0,detected=0;
-      foreach (string f in Directory.EnumerateFiles(usts, "*.*", SearchOption.AllDirectories).
-        Where((s) => (s.ToLower().EndsWith(".ust"))))
-      {
-        try
-        {
-          count.Add(zuio.GetEncUde(f,256));
-          detected++;
-        }
-        catch (Exception e)
-        {
-          Logger.Log(f);
-          Logger.Log(e.Message);
-          undetected++;
-        }
+      //string usts = @"D:\Test UST Archive";
+      //TallyDictionary<Encoding> count = new TallyDictionary<Encoding>();
+      //Stopwatch sw = new Stopwatch();
+      //sw.Start();
+      //int undetected = 0,detected=0;
+      //foreach (string f in Directory.EnumerateFiles(usts, "*.*", SearchOption.AllDirectories).
+      //  Where((s) => (s.ToLower().EndsWith(".ust"))))
+      //{
+      //  try
+      //  {
+      //    count.Add(zuio.GetEncUde(f,256));
+      //    detected++;
+      //  }
+      //  catch (Exception e)
+      //  {
+      //    Logger.Log(f);
+      //    Logger.Log(e.Message);
+      //    undetected++;
+      //  }
         
-      }
-      sw.Stop();
-      Logger.Log("Took " + sw.ElapsedMilliseconds + "ms total. Good files: "+detected+" Undetected files: "+undetected);
-      foreach(var e in count.Keys)
-        Logger.Log(e.EncodingName + "\t" + count[e]);
-      Logger.Save();
+      //}
+      //sw.Stop();
+      //Logger.Log("Took " + sw.ElapsedMilliseconds + "ms total. Good files: "+detected+" Undetected files: "+undetected);
+      //foreach(var e in count.Keys)
+      //  Logger.Log(e.EncodingName + "\t" + count[e]);
+      //Logger.Save();
     }
 
     [TestMethod]
+    [Ignore]
     public void TestUSTRead()
     {
-      string usts = @"D:\Test UST Archive";
-      List<USTFile> lib = new List<USTFile>();
-      Stopwatch sw = new Stopwatch();
-      sw.Start();
-      foreach (string f in Directory.EnumerateFiles(usts, "*.*", SearchOption.AllDirectories).
-        Where((s) => (s.ToLower().EndsWith(".ust"))))
-      {
-        Logger.Log(f);
-        lib.Add(new USTFile(f));
-      }
-      sw.Stop();
-      Logger.Log("Took " + sw.ElapsedMilliseconds + "ms total, " + lib.Count + " files, " + sw.ElapsedMilliseconds / lib.Count + "ms per file");
-      Logger.Save();
+      //string usts = @"D:\Test UST Archive";
+      //List<USTFile> lib = new List<USTFile>();
+      //Stopwatch sw = new Stopwatch();
+      //sw.Start();
+      //foreach (string f in Directory.EnumerateFiles(usts, "*.*", SearchOption.AllDirectories).
+      //  Where((s) => (s.ToLower().EndsWith(".ust"))))
+      //{
+      //  Logger.Log(f);
+      //  lib.Add(new USTFile(f));
+      //}
+      //sw.Stop();
+      //Logger.Log("Took " + sw.ElapsedMilliseconds + "ms total, " + lib.Count + " files, " + sw.ElapsedMilliseconds / lib.Count + "ms per file");
+      //Logger.Save();
       //System.Diagnostics.Process.Start(Path.Combine(Directory.GetCurrentDirectory(), "testout.ust"));
     }
 
     [TestMethod]
+    [Ignore]
     public void TestReadMrq()
     {
-      var dic = Mrq.GetDictionary(@"D:\SourceTree\UTAU-Interfacing API Library\Tests\dummyMrqs\desc.mrq");
+      //var dic = Mrq.GetDictionary(@"D:\SourceTree\UTAU-Interfacing API Library\Tests\dummyMrqs\desc.mrq");
 
-      foreach (var v in dic.Keys)
-        Logger.Log(v + "\t" + string.Join(", ", dic[v]));
+      //foreach (var v in dic.Keys)
+      //  Logger.Log(v + "\t" + string.Join(", ", dic[v]));
 
-      Logger.Save();
+      //Logger.Save();
       // var ent = Mrq.DoYourThingPlease(@"D:\SourceTree\UTAU-Interfacing API Library\Tests\dummyMrqs\desc.mrq");
     }
   }
